edda21a4322adac8237206ee181f0f2c4a3f272c
Mohamed Gumaa Abdalla Mohamed <gomaam14@gmail.com>
2021-01-16 21:07:11 +0400

    git user profile information

--- core/views.py
+++ core/views.py
@@ -1,33 +1,20 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from decouple import config
 import smartsheet
+from decouple import config
+
 
 # Initialize client and make sure we don't miss any errors
 user =  smartsheet.Smartsheet(config('API_KEY'))
-user.errors_as_exceptions(True)
-
 
 def index(request):
     user_profile = user.Users.get_current_user()
 
-    # Get all sheets
-    sheets = user.Sheets.list_sheets(include="attahments,source", include_all=True).data
-
     context = {
-        "sheets": sheets
+        'user_profile': user_profile,
     }
-    return render(request, "core/index.html", context)
 
-def view_sheet(request, id):
-    sheet = user.Sheets.get_sheet(id)
-    column_map = {column.title : column.id for column in sheet.columns}
+    return render(request, 'core/index.html', context)
 
-    data = []
-    # Get cells values
-    for row in sheet.rows:
-        cells = [row.get_column(col_id).value for col_id in column_map.values()]
-        print(cells)
-        data.append(cells)
+def view_sheet(request):
 
-    return JsonResponse({"column_map": column_map, "rows": data},safe=False)
+    return "hello"
