e5928b9f596ee2d2de99ecf693ac9f92d6c8fa44
Mohamed Gumaa Abdalla Mohamed <gomaam14@gmail.com>
2021-01-17 21:35:18 +0400

    initial filter for sheet

--- core/views.py
+++ core/views.py
@@ -15,19 +15,26 @@ def index(request):
     sheets = user.Sheets.list_sheets(include="attahments,source", include_all=True).data
 
     context = {
-        "sheets": sheets
+        'user_profile':  user_profile,
+        "sheets": sheets,
     }
     return render(request, "core/index.html", context)
 
 def view_sheet(request, id):
     sheet = user.Sheets.get_sheet(id)
     column_map = {column.title : column.id for column in sheet.columns}
-
     data = []
+
     # Get cells values
     for row in sheet.rows:
         cells = [row.get_column(col_id).value for col_id in column_map.values()]
-        print(cells)
         data.append(cells)
+
+    # Get all Task column value
+    for row in sheet.rows:
+        for col_title, col_id in column_map.items():
+            if col_title == 'Task':
+                print(row.get_column(col_id).display_value)
+
 
     return JsonResponse({"column_map": column_map, "rows": data},safe=False)
